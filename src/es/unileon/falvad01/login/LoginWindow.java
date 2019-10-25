c6a2f604eaa5682438b990df38f98d3bc4e7ebc7
falvad01 <falvad01@estudiantes.unileon.es>
2019-10-25 22:56:57 +0200

    Primer commit

---
+++ src/es/unileon/falvad01/login/LoginWindow.java
@@ -0,0 +1,132 @@
+package es.unileon.falvad01.login;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+
+public class LoginWindow extends JFrame {
+
+	Toolkit screen;
+
+	private static final int PWIDTH = 750;
+	private static final int PHEIGH = 348;
+
+	/* LOGIN */
+	JPanel loginPanel;
+	private JTextField loginUser;
+	private JPasswordField loginPassword;
+
+	public LoginWindow() {
+		getContentPane().setBackground(Color.WHITE);
+		setBackground(Color.WHITE);
+
+		screen = Toolkit.getDefaultToolkit();
+
+		setBounds(1024 / 4, 768 / 6, PWIDTH, PHEIGH);
+
+		setUndecorated(true);
+
+		setTitle("Login");
+
+		try {
+			initComponents();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initComponents() throws IOException {
+
+		getContentPane().setLayout(null);
+		listener list = new listener();
+
+		JButton crossButton = new JButton(new ImageIcon(LoginWindow.class.getResource("/resources/cross.png")));
+		crossButton.setBounds(720, 11, 15, 15);
+		getContentPane().add(crossButton);
+		crossButton.setBackground(null);
+		crossButton.setBorder(null);
+		crossButton.setOpaque(false);
+		crossButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+
+		JButton minButton = new JButton(new ImageIcon(LoginWindow.class.getResource("/resources/min.png")));
+		minButton.setBorder(null);
+		minButton.setBackground(null);
+		minButton.setBounds(690, 11, 15, 15);
+		minButton.setOpaque(false);
+		getContentPane().add(minButton);
+		minButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setExtendedState(JFrame.CROSSHAIR_CURSOR);
+			}
+		});
+
+		JLabel backgroundLabel = new JLabel(new ImageIcon(LoginWindow.class.getResource("/resources/fondo.jpg")));
+		backgroundLabel.setBounds(218, 0, 532, 348);
+		getContentPane().add(backgroundLabel);
+
+		loginPanel = new JPanel();
+		loginPanel.setBackground(Color.WHITE);
+		loginPanel.setBounds(0, 0, 750, 348);
+		getContentPane().add(loginPanel);
+		loginPanel.setLayout(null);
+		JLabel iconLabel = new JLabel(new ImageIcon(LoginWindow.class.getResource("/resources/icon.png")));
+		iconLabel.setBounds(23, 62, 45, 45);
+		loginPanel.add(iconLabel);
+
+		loginUser = new JTextField();
+		loginUser.setBackground(Color.WHITE);
+		loginUser.setBounds(83, 115, 115, 20);
+		loginPanel.add(loginUser);
+		loginUser.setColumns(10);
+
+		loginPassword = new JPasswordField();
+		loginPassword.setBounds(83, 146, 115, 20);
+		loginPanel.add(loginPassword);
+
+		JLabel lblUser = new JLabel("USER");
+		lblUser.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 11));
+		lblUser.setBounds(49, 118, 73, 14);
+		loginPanel.add(lblUser);
+
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 11));
+		lblPassword.setBounds(23, 146, 90, 14);
+		loginPanel.add(lblPassword);
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 11));
+		btnLogin.setBounds(125, 177, 73, 23);
+		loginPanel.add(btnLogin);
+		btnLogin.setBackground(null);
+		// btnLogin.setBorder(null);
+		btnLogin.setOpaque(false);
+		btnLogin.addActionListener(list);
+
+	}
+
+	public class listener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+
+			if (arg0.getActionCommand().equals("Login")) {
+
+				System.out.println(loginPassword.getPassword());
+
+			}
+		}
+	}
+}
---
+++ src/es/unileon/falvad01/login/main.java
@@ -0,0 +1,17 @@
+package es.unileon.falvad01.login;
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		LoginWindow window = new LoginWindow();
+
+		window.setVisible(true);
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+	}
+
+}
