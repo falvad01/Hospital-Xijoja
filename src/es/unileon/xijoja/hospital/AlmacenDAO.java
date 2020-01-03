a7565f9e1e9697c7b7ab21aedce631c1534c7a6e
xgarcn00 <xgarcn00@estudiantes.unileon.es>
2020-01-03 23:48:43 +0100

    añadido dao de almacen

---
+++ src/es/unileon/xijoja/hospital/AlmacenDAO.java
@@ -0,0 +1,34 @@
+package es.unileon.xijoja.hospital;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.swing.JTextField;
+
+import java.sql.PreparedStatement;
+ 
+/**
+ *
+ * @author Xijoja
+ *
+ */
+public class AlmacenDAO {
+ 
+	private Connection conn;
+    private Conexion co;
+ 
+    /**
+     *
+     */
+    public AlmacenDAO() {
+    }
+}
+ 
