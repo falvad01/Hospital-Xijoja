f1b01687645415b793913954a98b1508cb813f9e
Jgarcg40 <Jgarcg40@estudiantes.unileon.es>
2020-01-11 20:52:08 +0100

    comenzado resumen semana

--- src/es/unileon/xijoja/hospital/medicWindow/ControlerMedicWindow.java
+++ src/es/unileon/xijoja/hospital/medicWindow/ControlerMedicWindow.java
@@ -98,8 +98,8 @@ public class ControlerMedicWindow implements ActionListener {
 			}
 		}
 		
-		
-	}
+	}	
+
 	public boolean isDni (String dniOrRoom) {
 		// return true si es dni
 	
@@ -297,7 +297,7 @@ public class ControlerMedicWindow implements ActionListener {
 		if(add) {
 		//comprueba si se introduce un dni o numero de habitacion;
 		
-
+			
 			boolean isDniOrRoom = isDni(window.textFieldDNIToDelete.getText().toString());
 
 			if (!dao.checkPatientExist(window.textFieldDNIToDelete.getText().toString(),isDniOrRoom)) {
@@ -314,11 +314,12 @@ public class ControlerMedicWindow implements ActionListener {
 				while(it.hasNext())
 				 System.out.println(it.next());
 				*/
-					
+					Date date = new Date(Calendar.getInstance().getTime().getTime());// Obtenemos la fecha actual
 					dao.deletePatient(window.textFieldNameToDelete.getText().toString(),
 					window.textFieldFirstDeleteToDelete.getText().toString(),
 					window.textFieldSecondDeleteToDelete.getText().toString(), window.textFieldDNIToDelete.getText().toString());
 					window.lblErrorDelete.setText("");
+				
 
 			}
 		}
@@ -458,55 +459,6 @@ public class ControlerMedicWindow implements ActionListener {
 		window.addMedicine.setVisible(false);
 		window.week.setVisible(true);
 		
-		//para ver ue pacientes hay
-		/*
-		Iterator<String[]> it = arrayPacientes.iterator();
-		while(it.hasNext())
-		 System.out.println(it.next());
-		*/
-		
-		//copiad de ver pacientes, mi idea es con un arraylist guardar los eliminados y hacer una tabla
-		/*ArrayList<String[]> insert = null;
-
-		String[] titles = null;
-
-		String[][] matrixToInsert = null;
-
-		titles = new String[] { "  Id", "Nombre", "Apellido 1", "Apellido 2", "NIF", "Fecha", "Habitación",
-				"Enfermedad", "Producto", "Medico", "Unidades medicamento", "Enfermero " }; // Titulos de la tabla de
-																// los empleados
-		insert = dao.getAllPatients();// ArrayList de Arrays
-	
-		matrixToInsert = new String[insert.size() + 1][12];
-		window.seePacientsPanel.setPreferredSize(new Dimension(624, 20 + 20 * insert.size()));
-		window.seePacientsPanel.setBounds(284, 11, 624, 20 + 20 * insert.size());
-		
-		for (int i = 0; i < insert.size()+1; i++) { // rellenamos la matriz que meteremos en la tabla a partir
-			// del ArrayList de arrays devuelto del DAO
-			for (int j = 0; j < 12; j++) {
-				if (i == 0) {
-					
-
-					matrixToInsert[i][j] = titles[j];
-
-				} else {
-					matrixToInsert[i][j] = insert.get(i-1)[j];					}
-			}
-}
-		
-		JTable PatientsTable = new JTable();
-		PatientsTable.setBounds(5, 5, 600, 20 + 20 * insert.size());
-
-		PatientsTable.setVisible(true);
-		window.seePacientsPanel.add(PatientsTable);
-		PatientsTable.setAutoscrolls(true);
-		
-
-		
-		DefaultTableModel tableModel = new DefaultTableModel(matrixToInsert, titles);
-		PatientsTable.setModel(tableModel);
-		
-		*/
 		
 		
     }else if (arg0.getActionCommand().equals("Cerrar Sesión")) {
